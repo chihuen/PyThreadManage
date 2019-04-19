38c5211512d7fd69ccfdb402f2da368bea6c5ff3
chihuen <510547394@qq.com>
2019-04-20 06:18:59 +0800

    add setup.py, readme and license

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PyThreadManage",
+    version="0.1",
+    description="encapsulate functions of thread module",
+    license="MIT Licence",
+
+    url="https://github.com/chihuen/PyThreadManage",
+    author="Chihuen Lau",
+    author_email="chihuenlau@gmail.com",
+
+    packages=find_packages(),
+    platforms="any",
+    install_requires=[],
+
+    scripts=[],
+    entry_points={}
+)
